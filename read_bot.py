166b5e8b861936a937c644ea5b7be79c7ed4d028
yshi112358 <yshi112358@gmail.com>
2021-01-03 04:16:17 +0900

    コメントアウト

--- read_bot.py
+++ read_bot.py
@@ -7,8 +7,8 @@ from gtts import gTTS
 from voice_generator import creat_WAV
 import discord_send_error
 
-prefix = '?'
-#prefix = '.'
+prefix = '?'#dev
+#prefix = '.'#main
 
 client = commands.Bot(command_prefix = prefix)
 voice_client = None
@@ -79,5 +79,5 @@ async def on_message(message):
         import traceback
         discord_send_error.send_error_log(traceback.format_exc())
 
-client.run("Nzk0OTQxMzU1MDc0NzgxMjU0.X_CI1A.4wTuK0UhfprkJKTJGNy_4Iuy4aY")
-#client.run("Nzk0NTY1MTE3OTc4NDc2NTU0.X-8qbg.hoWlswUZztYE0pYM5e9clscITtQ")
+client.run("Nzk0OTQxMzU1MDc0NzgxMjU0.X_CI1A.4wTuK0UhfprkJKTJGNy_4Iuy4aY")#dev
+#client.run("Nzk0NTY1MTE3OTc4NDc2NTU0.X-8qbg.hoWlswUZztYE0pYM5e9clscITtQ")#main
