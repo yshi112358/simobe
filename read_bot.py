ee95ed630b171070c34dc1c2e29a7b7ca5d37fed
yshi112358 <yshi112358@gmail.com>
2021-01-03 03:53:48 +0900

    プレフィックスの変数化

--- read_bot.py
+++ read_bot.py
@@ -7,7 +7,12 @@ from gtts import gTTS
 from voice_generator import creat_WAV
 import discord_send_error
 
-client = commands.Bot(command_prefix='?')
+prefix = '?'
+#prefix = '.'
+client.run("Nzk0OTQxMzU1MDc0NzgxMjU0.X_CI1A.4wTuK0UhfprkJKTJGNy_4Iuy4aY")
+#client.run("Nzk0NTY1MTE3OTc4NDc2NTU0.X-8qbg.hoWlswUZztYE0pYM5e9clscITtQ")
+
+client = commands.Bot(command_prefix=prefix)
 voice_client = None
 
 
@@ -59,7 +64,7 @@ async def on_message(message):
         msgclient = message.guild.voice_client
         print(msgclient)
         print(discord.opus.is_loaded())
-        if message.content.startswith('?'):
+        if message.content.startswith(prefix):
             pass
 
         else:
@@ -75,5 +80,3 @@ async def on_message(message):
     except:
         import traceback
         discord_send_error.send_error_log(traceback.format_exc())
-
-client.run("Nzk0OTQxMzU1MDc0NzgxMjU0.X_CI1A.4wTuK0UhfprkJKTJGNy_4Iuy4aY")
