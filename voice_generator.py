83d3d8a8c87da969720ca30070ab309495e69f66
yshi112358 <yshi112358@gmail.com>
2021-01-03 17:01:07 +0900

    input.txtの有無

--- voice_generator.py
+++ voice_generator.py
@@ -76,18 +76,16 @@ def user_custam(text):
 # 書き込みファイル：input.txt、output.wav
 # ************************************************
 def creat_WAV(inputText):
-        # message.contentをテキストファイルに書き込み
-
     inputText = remove_custom_emoji(inputText)   # 絵文字IDは読み上げない
     inputText = remove_command(inputText)   # コマンドは読み上げない
     inputText = url_shouryaku(inputText)   # URLなら省略
     inputText = remove_picture(inputText)   # 画像なら読み上げない
     inputText = remove_log(inputText)   # 参加ログなら読み上げない
     #inputText = user_custam(inputText)   # ユーザ登録した文字を読み替える
-    input_file = 'input.txt'
+    #input_file = 'input.txt'
 
-    with open(input_file,'w',encoding='shift_jis') as file:
-        file.write(inputText)
+    #with open(input_file,'w',encoding='shift_jis') as file:
+    #    file.write(inputText)
 
     tts = gTTS(text=inputText, lang='ja')
     tts.save('./output.mp3')
