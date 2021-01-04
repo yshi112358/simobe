7261f7dec7cf1dd320dc86fd6d521466ccca2ad3
yshi112358 <yshi112358@gmail.com>
2021-01-05 01:15:00 +0900

    自動退出機能

--- read_bot.py
+++ read_bot.py
@@ -2,20 +2,18 @@ import discord
 from discord.ext import commands
 import asyncio
 import os
-import subprocess
 from gtts import gTTS
 from voice_generator import creat_MP3
 import discord_send_error
 import time
 from mutagen.mp3 import MP3
+import traceback
 
 prefix = os.environ["prefix"]
 
-
 client = commands.Bot(command_prefix = prefix)
 
-voice_client = None
-
+#ログイン処理
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -23,26 +21,24 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-
+#入室
 @client.command()
 async def join(ctx):
+    global ctx_join
     print('#join')
-    print('#voicechannelを取得')
     vc = ctx.author.voice.channel
-    print('#voicechannelに接続')
     await vc.connect()
     await ctx.channel.send('接続されたよ！')
-    with open('./text_channel.txt',mode='w',encoding="shift-jis") as f:
-        f.write(str(ctx.channel))
+    ctx_join = ctx
 
+#退室
 @client.command()
 async def bye(ctx):
     print('#bye')
-    print('#切断')
-    print(type(ctx))
     await ctx.channel.send('またね！')
     await ctx.voice_client.disconnect()
 
+#辞書登録
 @client.command()
 async def register(ctx, arg1, arg2):
     with open('./dic.txt', mode='a',encoding='shift-jis') as f:
@@ -50,40 +46,37 @@ async def register(ctx, arg1, arg2):
         print('dic.txtに書き込み：''\n'+ arg1 + ',' + arg2)
     await ctx.send('`' + arg1+'` を `'+arg2+'` として登録しました！')
 
+#自動退出
 @client.event
 async def on_voice_state_update(member, before, after):
-    print(type(after))
-    #if after.channel is None:
-        #await member.channel.send('またね！')
-        #await before.voice_client.disconnect()
+    if member.bot:
+        return
+    if after.channel is None:
+        await ctx_join.channel.send('人がいなくなっちゃったから、ばいばい！')
+        await ctx_join.voice_client.disconnect()
 
+#発声
 @client.event
 async def on_message(message):
-    if message.author.bot:
-        return
-    with open('./text_channel.txt',mode='r',encoding='shift-jis') as f:
-        text_channel = f.read()
     try:
+        if message.author.bot:
+            return
         print('---on_message_start---')
         if message.content.startswith(prefix):
             pass
-        else:
-            if str(message.channel) == text_channel:
-                if message.guild.voice_client:
-                    print("channel:" + str(message.channel))
-                    print("speaker:" + str(message.author.display_name))
-                    play_MP3(message,message.author.display_name,"output_name.mp3")
-                    print('content:' + str(message.content))
-                    play_MP3(message,message.content,"output_content.mp3")
-                else:
-                    pass
-
+        elif message.channel == ctx_join.channel:
+            if message.guild.voice_client:
+                print("channel:" + str(message.channel))
+                print("speaker:" + str(message.author.display_name))
+                play_MP3(message,message.author.display_name,"output_name.mp3")
+                print('content:' + str(message.content))
+                play_MP3(message,message.content,"output_content.mp3")
         await client.process_commands(message)
         print('---on_message_end---')
     except:
-        import traceback
         discord_send_error.send_error_log(traceback.format_exc())
 
+#発声モジュール
 def play_MP3(message,inputText,file_name):
     creat_MP3(inputText,file_name)
     source = discord.FFmpegOpusAudio(file_name)
