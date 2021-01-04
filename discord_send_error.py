79e442aca47c53ffef87c4652fb92a2f41bd4286
yshi112358 <yshi112358@gmail.com>
2021-01-04 15:41:21 +0900

    環境変数と連投への対応

--- discord_send_error.py
+++ discord_send_error.py
@@ -1,7 +1,7 @@
 ﻿import requests
+import os
 
-web_url = 'https://discord.com/api/webhooks/795000657589174313/20jq90xAz6IgL3FDA-KFrlgzIYaSJc6tv4EBYce2dbkr7DQ7_hlfl4qcmflmNDEy-4_r'#dev
-#web_url = 'https://discord.com/api/webhooks/794963895663329280/qyv3HT-Jf47znqXX5I0J7ZdENNGVR3jTPZcoHRp1PIg8WD9Iq_Y-w3NFGXyXYTwieosK'#main
+web_url = os.environ["web_url"]
 
 
 def send_error_log(message):
