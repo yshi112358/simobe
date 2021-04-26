5d14a842c781cc2fa8eea3d65e9ad6269a131d85
yshi112358 <yshi112358@gmail.com>
2021-04-26 21:06:30 +0900

    Update read_bot.py

--- read_bot.py
+++ read_bot.py
@@ -133,6 +133,7 @@ async def a(ctx,arg,*member_count):
         for member in bot_vc.members:
             await member.edit(mute=False)
         for member in member_count:
-            await ctx.guild.voice_client.move_to(client.get_channel(amongus_ghost))
+            print(client.get_channel(amongus_ghost))
+            //await ctx.guild.voice_client.move_to(client.get_channel(amongus_ghost))
 
 client.run(os.environ["client"])
