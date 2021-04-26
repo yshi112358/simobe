73e3120e0f387cc4f0a6c6dbde346f2b33996f2e
yshi112358 <yshi112358@gmail.com>
2021-04-27 00:42:59 +0900

    Update read_bot.py

--- read_bot.py
+++ read_bot.py
@@ -147,7 +147,7 @@ async def a(ctx,arg,*member_count):
             await member.edit(mute=False)
         for member in member_list:
             await member.move_to(amongus_room)
-    elif
+    else:
         await ctx.channel.send("Among Usモードへようこそ！\n「"+prefix+"a」でいつでも操作方法を見ることができます。\n\n")
 
 client.run(os.environ["client"])
